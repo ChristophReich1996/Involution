82c5265b86bdd81f6f9f334f39909d9cd44f1275
Christoph Reich <34400551+ChristophReich1996@users.noreply.github.com>
2021-03-12 02:34:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 
-requirements = [
-    'torch>=1.7.0'
-]
-
 setup(
     name="involution",
     version="0.1",
     url="https://github.com/ChristophReich1996/Involution",
-    license='MIT License',
+    license="MIT License",
     author="Christoph Reich",
-    author_email='ChristophReich@gmx.net',
+    author_email="ChristophReich@gmx.net",
     description="PyTorch 2d Involution",
-    install_requires=requirements,
+    install_requires=["torch>=1.7.0"],
 )
